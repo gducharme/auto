bea0d74bc98f33b48820e7594716f87c4fda1cb3
Geoffrey Ducharme <37793+gducharme@users.noreply.github.com>
2025-07-14 10:49:26 -0400

    Add SQLAlchemy setup

---
+++ db.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # loads DATABASE_URL
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./substack.db")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args=(
+        {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+    ),
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
