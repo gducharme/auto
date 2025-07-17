6c0c00816cd3ab251d77e5bbbb74718c177fd8d4
Geoffrey L'Amour <geoff.ducharme@icloud.com>
2025-07-17 09:20:43 -0400

    add exp1

---
+++ src/experiments/dspy.py
@@ -0,0 +1,9 @@
+import dspy
+
+lm = dspy.LM("ollama_chat/llama3.2:1b", api_base="http://localhost:11434", api_key="")
+dspy.configure(lm=lm)
+
+lm("Say this is a test!", temperature=0.7)  # => ['This is a test!']
+lm(
+    messages=[{"role": "user", "content": "Say this is a test!"}]
+)  # => ['This is a test!']
